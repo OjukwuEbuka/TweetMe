620da8a381b941020b355a41c680365029680774
OjukwuEbuka <ebukaojukwu23@gmail.com>
2020-07-15 11:49:43 +0100

    Created ModelForm for create-tweet

--- tweets/views.py
+++ tweets/views.py
@@ -1,12 +1,24 @@
 from random import randint
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render, Http404
+from django.shortcuts import render, Http404, redirect
 
+from .forms import TweetForm
 from .models import Tweet
 # Create your views here.
 def home_view(request, *args, **kwargs):
     # return HttpResponse('Hello World')
     return render(request, "pages/home.html", context={}, status=200)
+
+def tweet_create_view(request, *args, **kwargs):
+    form = TweetForm(request.POST or None)
+    next_url = request.POST.get("next") or None
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.save()
+        if next_url != None:
+            return redirect(next_url)
+        form = TweetForm()
+    return render(request, "components/form.html", context={"form":form})
 
 def tweet_list_view(request, *args, **kwargs):
     """
