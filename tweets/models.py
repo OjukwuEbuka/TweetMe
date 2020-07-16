dddd61b8ae7fac9ee5a3c5e21cff0be1a2aafd59
OjukwuEbuka <ebukaojukwu23@gmail.com>
2020-07-16 22:53:38 +0100

    Changed form to Django RestFramework serializer

--- tweets/models.py
+++ tweets/models.py
@@ -1,14 +1,21 @@
 from django.db import models
+from django.conf import settings
 from random import randint
 
 # Create your models here.
 
+User = settings.AUTH_USER_MODEL
+
 class Tweet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     image = models.FileField(upload_to='images/', blank=True, null=True)
     
     class Meta:
         ordering = ['-id']
+
+    def __str__(self):
+        return(f"Tweet-{self.id}")
 
     def serialize(self):
         return {
--- tweets/views.py
+++ tweets/views.py
@@ -6,6 +6,7 @@ from django.utils.http import is_safe_url
 
 from .forms import TweetForm
 from .models import Tweet
+from .serializers import TweetSerializer
 
 ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 # Create your views here.
@@ -14,12 +15,26 @@ def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
 
 def tweet_create_view(request, *args, **kwargs):
+    serializer = TweetSerializer(data=request.POST or None)
+    if serializer.is_valid():
+        serializer.save(user=request.user)
+    return JsonResponse(serializer.data, status=201)
+
+
+def tweet_create_view_pure_django(request, *args, **kwargs):
+    user = request.user
+    if not request.user.is_authenticated:
+        user = None
+        if request.is_ajax():
+            return JsonResponse({}, status=401)
+        return redirect(settings.LOGIN_URL)
     form = TweetForm(request.POST or None)
     next_url = request.POST.get("next") or None
     # print(f"AJAX request {request.is_ajax()}")
 
     if form.is_valid():
         obj = form.save(commit=False)
+        obj.user = user
         obj.save()
         if request.is_ajax():
             # print("AJAX request")
